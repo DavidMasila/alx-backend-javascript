c193ace840009ef49b0d312e0ce5825d26c95a05
MasilaDavid <davidmasila97@gmail.com>
2022-12-25 03:01:24 -0800

    JS BACKEND

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    for (let x of array) {
+      let value = array[x];
+      array[x] = appendString + value;
+    }
+  
+    return array;
+  }
