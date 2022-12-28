82f24bf8025356d8fe2209e519772fcdefd3b100
MasilaDavid <davidmasila97@gmail.com>
2022-12-28 05:28:34 -0800

    JS BACKEND

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromApit(success){
+    return new Promise((resolve, reject) => {
+        if(success){
+            resolve({
+                status: 200,
+                bosy: 'Success'
+            });
+        } else {
+            reject('The fake API is no working currently');
+        }
+    });
+}
