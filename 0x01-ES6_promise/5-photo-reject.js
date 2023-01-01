3566687babeb2d955224ec1ec16c0caa8f07e0ea
MasilaDavid <davidmasila97@gmail.com>
2023-01-01 04:55:06 -0800

    JS BACKEND

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(fileName) {
-    return Promise.reject((err) => {
-        console.log(err + ' ' + '$fileName cannot be processed');
-    });
+    return Promise.reject(console.log(err + ' ' + '$fileName cannot be processed'));
 }
