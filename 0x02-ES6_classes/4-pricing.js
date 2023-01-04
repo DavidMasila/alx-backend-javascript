8b42903e63297a95275f2f36787cc4c056084b75
MasilaDavid <davidmasila97@gmail.com>
2023-01-04 08:35:41 -0800

    JS BACKEND

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,41 @@
 import Currency from './3-currency';
 
 export default class Pricing {
-  constructor(amount, currency) {
+  constructor (amount, currency) {
+    if (typeof amount !== 'number') throw new TypeError('Amount must be a number');
+    if (!(currency instanceof Currency)) throw new TypeError('Currency must be an instance of class Currency');
+
     this._amount = amount;
     this._currency = currency;
   }
 
-  get amount() {
+  get amount () {
     return this._amount;
   }
 
-  get currency() {
+  get currency () {
     return this._currency;
   }
 
-  set amount(value) {
-    if (typeof amount === 'number') this._amount = value;
+  set amount (value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
   }
 
-  set currency(value) {
-    if (currency instanceof Currency) this._currency = value;
+  set currency (value) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of class Currency');
+    }
+    this._currency = value;
   }
 
-  displayFullPrice() {
-    return `${this._amount} ${this._name} (${this._code})`;
+  displayFullPrice () {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
-  static convertPrice(amount, conversionRate) {
+  static convertPrice (amount, conversionRate) {
     return amount * conversionRate;
   }
 }
