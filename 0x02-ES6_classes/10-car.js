1e7d00506b959f59283967c88bacfc6fda414091
MasilaDavid <davidmasila97@gmail.com>
2023-01-04 13:17:19 -0800

    JS BACKEND

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,11 +1,16 @@
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = symbol(brand);
-    this._motor = symbol(motor);
-    this._color = sysmbol(color);
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return `${this._brand} ${this._motor} ${this._color}`;
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
   }
 }
