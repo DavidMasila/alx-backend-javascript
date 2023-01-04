2255c86e460560244d0c02cbff4c6fe83383ca4c
MasilaDavid <davidmasila97@gmail.com>
2023-01-04 07:09:55 -0800

    JS BACKEND

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-
-  if (typeof name !== 'string') {
-    throw new TypeError ('Name must be a string');
-  } else if (typeof length !== 'number') {
-    throw new TypeError ('Length must be a number');
-  } else if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
-    throw new TypeError ('Students must be an array of strings');
-  }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else if (!Array.isArray(students) || !students.every((e) => typeof e === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
 
     this._name = name;
     this._length = length;
