c65f159da55ab5fc647b239bf180614aaf81c6f0
MasilaDavid <davidmasila97@gmail.com>
2023-01-08 08:06:51 -0800

    JS BACKEND

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
   if (Array.isArray(list, newGrades) && typeof city === 'string') {
-    const newList = list.filter((element) => element.city === city);
-    const finalList = newList.map((element) => {
-      if (newGrades.studentId === element.id) {
-        element.grade = newGrades.grade;
-      } else {
-        element.grade = 'NA';
-      }
-      return finalList;
-    });
+    const newList = list.filter((element) => element.location === city)
+      .map((element) => {
+        if (newGrades.id === element.id) {
+          element.grade = newGrades.grade;
+        } else {
+          element.grade = 'NA';
+        }
+      });
   }
   return [];
 }
