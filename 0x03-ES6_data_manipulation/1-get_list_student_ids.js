d0bcb79d2a1b7f00ed0e8c025e194ec786972bac
MasilaDavid <davidmasila97@gmail.com>
2023-01-08 03:21:08 -0800

    JS BACKEND

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+export default function getListStudentIds(list) {
+  if (Array.isArray(list)) {
+    const listofIds = [];
+    for (let i = 0; i < list.length; i++) {
+      listofIds.push(list[i].id);
+    }
+  } else {
+    return [];
+  }
+}
