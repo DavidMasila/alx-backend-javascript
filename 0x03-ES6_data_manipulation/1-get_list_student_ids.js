ea68241ac99be352afe60614804f7f614c31a1c7
MasilaDavid <davidmasila97@gmail.com>
2023-01-08 03:33:49 -0800

    JS BACKEND

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,9 @@
 export default function getListStudentIds(list) {
   if (Array.isArray(list)) {
-    const listofIds = [];
-    for (let i = 0; i < list.length; i++) {
-      listofIds.push(list[i].id);
-    }
-
-    return listofIds;
+    const newMap = list.map((element) => {
+        return element.id
+    })
+    return newMap 
   } else {
     return [];
   }
