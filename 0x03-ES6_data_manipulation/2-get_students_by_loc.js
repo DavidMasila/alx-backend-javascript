85fff5370c74e7813ec7a1b397e31eb5328719a2
MasilaDavid <davidmasila97@gmail.com>
2023-01-08 03:51:45 -0800

    JS BACKEND

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getStudentsByLocation(list, city) {
+  if (Array.isArray(list) && typeof city === 'string') {
+    const newList = list.filter((element) => element.location === city);
+    return newList;
+  }
+}
