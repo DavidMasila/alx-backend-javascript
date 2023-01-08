e30b79708b88a0d4f5de387318039d1b96707531
MasilaDavid <davidmasila97@gmail.com>
2023-01-08 04:02:08 -0800

    JS BACKEND

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,8 +1,6 @@
 export default function getStudentIdsSum(list) {
   if (Array.isArray(list)) {
-    const totalIds = list.reduce((total, element) => {
-      return total + element;
-    });
+    const totalIds = list.reduce((total, element) => total + element.id);
     return totalIds;
   }
   return [];
